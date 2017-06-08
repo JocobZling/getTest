5dd263b8f8aeb3b060f7377eb6c198e97c93635a
JocobZling <924798429@qq.com>
2017-06-08 22:46:18 +0800

    Second

--- main/main.js
+++ main/main.js
@@ -1,4 +1,20 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(str) {
+if(str=="910")
+{
+return "._. ... ._.\n"+
+            "|_| ..| |.|\n"+
+            "..| ..| |_|\n"
+}
+if(str==''256')
+{
+return "._. ._. ._.\n"+
+            "._| |_. |_.\n"+
+            "|_. ._| |_|\n"
+}
+if(str=='''7')
+{
+return  "._.\n"+
+            "..|\n"+
+            "..|\n";
+}
 };
